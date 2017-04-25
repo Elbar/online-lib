d0e923ef124fe94aa6661142c0db57a8764e3a13
Elbar <vista-e@mail.ru>
2017-04-25 10:46:42 +0600

    yooo

--- books/views.py
+++ books/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def index_view(request):
+    context = {}
+    template = "index.html"
+    return render(request, template, context)
